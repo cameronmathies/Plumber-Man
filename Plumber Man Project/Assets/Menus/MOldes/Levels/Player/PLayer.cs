fe9a46447887489d71a4598ff9e91ed5a6a11a87
cameronmathies <cmathies@IT202.mr.preble.com>
2015-10-23 13:37:36 -0400

    Move

--- Plumber Man Project/Assets/Menus/MOldes/Levels/Player/PLayer.cs
+++ Plumber Man Project/Assets/Menus/MOldes/Levels/Player/PLayer.cs
@@ -2,17 +2,37 @@
 using System.Collections;
 
 public class PLayer : MonoBehaviour {
+	public float moveSpeed;
+	public GameObject deathParticles;
+	public float Speed = 1f;
+	public float Force = 1f;
+	public float hitForce;
+	
+	private Vector3 input;
+	private Vector3 spawn;
+		FixedUpdate(){
 
-	void Move(){
-		if (Input.GetKey(KeyCode.UpArrow)) {
-			transform.Translate (Vector3.up);
-			Debug.Log ("UpArrow makes me go up");
-			}
-			if (Input.GetKey(KeyCode.W)) {
-			transform.Translate (Vector3.up);		
-			Debug.Log ("W makes me go up");
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			transform.Translate (Vector3.left * Time.deltaTime * moveSpeed, Camera.main.transform);
 		}
+
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			transform.Translate (Vector3.down * Time.deltaTime * 7, Camera.main.transform);
+		}
+
+			if (Input.GetKey (KeyCode.DownArrow)) {
+				transform.Translate (Vector3.down * Time.deltaTime * 7, Camera.main.transform);
+		}
+
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			transform.Translate (Vector3.left * Time.deltaTime * moveSpeed, Camera.main.transform);
 			}
 
+
+
+
+
+
+	}
 }
 
