15d9494e54fd14b073f6139c382c378f65767ea4
cameronmathies <cmathies@IT202.mr.preble.com>
2015-10-23 13:16:53 -0400

    Player Move

---
+++ Plumber Man Project/Assets/Menus/MOldes/Levels/Player/PLayer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PLayer : MonoBehaviour {
+
+	void Move(){
+		if (Input.GetKey(KeyCode.UpArrow)) {
+			transform.Translate (Vector3.up);
+			Debug.Log ("UpArrow makes me go up");
+			}
+			if (Input.GetKey(KeyCode.W)) {
+			transform.Translate (Vector3.up);		
+			Debug.Log ("W makes me go up");
+		}
+			}
+
+}
+
